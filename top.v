9be6b6582abc3e8bd45e325b36004ae57d238323
Caleb Cook <calebcook766@u.boisestate.edu>
2025-04-01 11:12:12 -0600

    Update top.v

--- top.v
+++ top.v
@@ -1,1 +1,45 @@
 
+module top(
+
+input [3:0] sw,
+input btnC, 
+output[5:0] led
+
+);
+
+
+D_Flip_Flop tops (
+
+.D(sw[0]), 
+.Q(led[0]),
+.NotQ(led[1]),
+.clk(btnC)
+
+);
+
+JK_Flip_Flop tops2(
+
+.J(sw[1]), 
+.K(sw[2]),
+.NotQ(led[3]),
+.Clock(btnC),
+.Q(led[2])
+
+);
+
+T_Flip_Flop Tops3 (
+
+
+.T(sw[3]), 
+.Clock(btnC),
+.NotQ(led[5]),
+.Q(led[4])
+
+
+);
+
+
+
+
+endmodule
+
